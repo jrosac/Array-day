5bd1f242fe492e98daaaa90357e0febaa7bd4343
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-03-13 00:51:09 -0300

    Added 3 ways to the third exercise (3/8)

--- script.js
+++ script.js
@@ -38,6 +38,22 @@ const inventors = [
 
     // Array.prototype.sort()
     // 3. Sort the inventors by birthdate, oldest to youngest
+    const ordered1 = inventors.sort(function(a,b){
+      if(a.year>b.year)
+         return 1
+      else
+        return -1
+    })
+    console.table(ordered1)
+    ///////////////////////////////////////////////
+    
+    const ordered2 = inventors.sort(function(a,b){
+      return a.year-b.year
+    })
+    console.table(ordered2)
+    ///////////////////////////////////////////////
+    const ordered3 = inventors.sort((a,b)=>a-b)
+    console.table(ordered3)
 
     // Array.prototype.reduce()
     // 4. How many years did all the inventors live all together?
