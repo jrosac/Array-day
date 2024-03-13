e9d7617295762506f3ab5fa9ce0d86314b800e96
jrosac <joaorosa.conceicao@dcomp.ufs.br>
2024-03-13 00:20:07 -0300

    Added the first exercise (1/8)

--- script.js
+++ script.js
@@ -23,6 +23,11 @@ const inventors = [
 
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
+    const fifteen = inventors.filter(function (inventor){
+      if(inventor.year>=1500 && inventor.year<1600)
+      return true
+    });
+    console.table(fifteen)
 
     // Array.prototype.map()
     // 2. Give us an array of the inventors first and last names
